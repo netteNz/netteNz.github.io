3c3e35f3d3fd14f5b86f202580e21b2a114acf93
Emanuel Lugo <lugo.emanuel@gmail.com>
2025-03-30 05:38:32 -0400

    #

--- projects/earthquake-map/main.py
+++ projects/earthquake-map/main.py
@@ -11,7 +11,7 @@ try:
                     zoom_start=9,
                     tiles='cartodbdark_matter',
                     prefer_canvas=True,
-                    height='600px'  # Use a specific pixel value instead of calc()
+                    #height='600px'  # Use a specific pixel value instead of calc()
                   )
     
     pr.get_name()  # Force initialization
